95f8ba737ce764508ac9fae458dbeb750900e400
Zohaib Khaliq <khaliqzohaib92@gmail.com>
2020-06-15 14:40:22 -0400

    Complete register after test with comments

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,28 +8,38 @@ const validateRegisterInput = require('../../validation/register');
 
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+
 // User registration
 router.post('/register', (req, res) => {
     
+    //check validation
     const {errors, isValid} = validateRegisterInput(req.body);
     if(!isValid) {
         return res.status(400).json(errors);
     }
 
+    //find if user already exist
     User.findOne({email: req.body.email})
     .then(user=>{
         if(user){
             return res.status(400).json("This email address is already exists")
         } else {
+            //create new user
             const newUser = new User({
                 email: req.body.email,
                 username: req.body.username,
                 password: req.body.password
             });
 
+            //create bcrypt salt
             bcrypt.genSalt(12, (err, salt) => {
+                //create bcrypt hash
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    //throw error
                     if(err) throw err;
+
+                    //make new user
                     newUser.password = hash;
                     newUser.save()
                     .then(user => {
@@ -38,6 +48,7 @@ router.post('/register', (req, res) => {
                             username: user.username
                         };
 
+                        //sign jwt token 
                         jwt.sign(
                             payload,
                             keys.secretOrKey,
