bbdef394bac9d63cf72a69976da17bdee1fefc21
Danny Morgan <djmorgan2412@gmail.com>
2020-06-16 14:08:32 -0400

    added categories

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
 const users = require("./routes/api/users");
 const hunts = require('./routes/api/hunts');
+const categories = require('./routes/api/categories');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -27,5 +28,7 @@ require('./config/passport')(passport);
 app.get("/", (req, res) => res.send("Get Set Hello"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use("/api/users", users);
+app.use("/api/categories", categories);
+
 
 
