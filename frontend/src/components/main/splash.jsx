b2a5f256fb3b8043ee259c47889976325b2c74af
Isaac Owens <isaac.owens832@gmail.com>
2020-06-17 18:43:59 -0400

    Install fonteawesome dependencies and package

--- frontend/src/components/main/splash.jsx
+++ frontend/src/components/main/splash.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 class Splash extends React.Component {
 
     render() {
+        let forwardIcon = <FontAwesomeIcon icon={faChevronRight} size="3x" />;
+        let backIcon = <FontAwesomeIcon icon={faChevronLeft} size="3x" />;
+    
         return (
           <div className="splash">
             <div className="splash-header">
               <h1>Welcome to GetSet!</h1>
             </div>
             <div className="splash-body">
-              <div className="splash-icon left">icon</div>
-              <div className="splash-image">image here</div>
-              <div className="splash-icon right">icon</div>
+                <div className="splash-icon left">{backIcon}</div>
+                <div className="splash-image">image here</div>
+                <div className="splash-icon right">{forwardIcon}</div>
             </div>
           </div>
         );
