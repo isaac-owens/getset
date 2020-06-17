145bba85a882d623c5e84b4043383a448e76cd27
Isaac Owens <isaac.owens832@gmail.com>
2020-06-17 09:14:55 -0400

    Fix nonpersisted changes from previous post deploy state

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+const path = require('path');
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
