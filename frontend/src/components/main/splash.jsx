1b833f7dc9a2f327960401f1231f83bfe7a46b36
Isaac Owens <isaac.owens832@gmail.com>
2020-06-17 18:18:21 -0400

    Finish positioning of splash page elements

--- frontend/src/components/main/splash.jsx
+++ frontend/src/components/main/splash.jsx
@@ -4,8 +4,16 @@ class Splash extends React.Component {
 
     render() {
         return (
-            <div>
+          <div className="splash">
+            <div className="splash-header">
+              <h1>Welcome to GetSet!</h1>
             </div>
+            <div className="splash-body">
+              <div className="splash-icon left">icon</div>
+              <div className="splash-image">image here</div>
+              <div className="splash-icon right">icon</div>
+            </div>
+          </div>
         );
     }
 }
