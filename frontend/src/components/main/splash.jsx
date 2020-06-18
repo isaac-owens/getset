4b6fd34164323686f6dfde979e394e5a725244f4
Isaac Owens <isaac.owens832@gmail.com>
2020-06-18 19:05:11 -0400

    Refactor splash page to take out carousel

--- frontend/src/components/main/splash.jsx
+++ frontend/src/components/main/splash.jsx
@@ -16,6 +16,17 @@ class Splash extends React.Component {
               <h1>Welcome to GetSet!</h1>
             </div>
             <div className="splash-body">
+              <div className="splash-content card-styling">
+                <p>
+                  Lorem ipsum is a placeholder text. It is commonly used by designers and developers to create the document or website content without having the original content.
+                  It serves the purpose of material even though the version is not meaningful. Replacing the actual content with placeholder text allows designers to design the layout 
+                  properly even before the content itself has been produced. It is used to create documents or paragraphs as required according to your content requirement.
+                  Also though there is not much importance in the variety of such placeholders, you can try some different styles of it.
+                  The ‘lorem ipsum’ text is from a famous passage by Cicero of Ancient Rome and words are added, altered and removed to make it nonsensical in improper Latin. It has been 
+                  used as placeholder text by typesetters since the 16th century, and this tradition continues on the web.
+                  Lipsum
+                </p>
+              </div>
               <div className="splash-image">
                 image here
               </div>
