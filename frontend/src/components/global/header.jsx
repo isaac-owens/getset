a9df74010702d96851b8fa66a638787a7722e2dd
Kevin.K <kevinkusky@gmail.com>
2020-06-18 14:53:13 -0400

    Fix vh settings, style links, default styling for buttons

--- frontend/src/components/global/header.jsx
+++ frontend/src/components/global/header.jsx
@@ -17,25 +17,33 @@ class Header extends React.Component {
     getLinks() {
         if (this.props.currentUser) {
             return (
-                <div >
-                    <ul className="header-nav-links">Hi {this.props.currentUser.username}!
-                        <li>
-                            <Link to={CREATE}>Create-Hunt </Link>
-                        </li>
-                        <li>
-                            <Link to={INDEX}>See-Hunts </Link>
-                        </li>
-                        <li>
-                            <Link to={CINDEX}>See-Challenges </Link>
-                        </li>
-                        {/* <li>
+              <div>
+                <span className="header-hello">
+                  Hi {this.props.currentUser.username}!
+                </span>
+                <ul className="header-nav-links">
+                  <li>
+                    <Link to={CREATE}
+                    className="header-nav-link">
+                    Create-a-Hunt
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={INDEX}
+                    className="header-nav-link">Hunts</Link>
+                  </li>
+                  <li>
+                    <Link to={CINDEX}
+                    className="header-nav-link">Challenges</Link>
+                  </li>
+                  {/* <li>
                             <Link to={STATS}>Stats</Link>
                         </li> */}
-                        <li>
-                            <button onClick={this.logoutUser}>Logout</button>
-                        </li>
-                    </ul>
-                </div>
+                  <li>
+                    <button onClick={this.logoutUser}>Logout</button>
+                  </li>
+                </ul>
+              </div>
             );
         } else {
             return (
