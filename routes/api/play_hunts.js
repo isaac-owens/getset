ca250242059a0ed745240bcdf53809685be257eb
Danny Morgan <djmorgan2412@gmail.com>
2020-06-18 16:56:32 -0400

    Finish image comparison

--- routes/api/play_hunts.js
+++ routes/api/play_hunts.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
@@ -20,33 +22,33 @@ router.get("/test", (req, res) => res.json({ msg: "This is the play hunts route"
 router.post("/", [passport.authenticate('jwt', { session: false }), upload.array('images', 10)], (req, res) => {
     // fetch hunt based on hunt_id
     let arr = [];
-    let arg;
     Hunt.findById(req.body.hunt_id)
         .then(playHunt => {
             // debugger
             // validating based on hunt images
             const { errors, isValid } = validatePlayHuntInput(req, playHunt);
             if (!isValid) return res.status(400).json(errors);
-            
+
             let s3bucket = new AWS.S3({
                 accessKeyId: process.env.AWS_ACCESS_KEY_ID || keys.AWS_ACCESS_KEY_ID,
                 secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || keys.AWS_SECRET_ACCESS_KEY,
                 region: process.env.AWS_REGION || keys.AWS_REGION
             });
-            
+
             const numFiles = req.files.length;
-            const imageAwsPath = [];
+            // const imageAwsPath = [];
+            const imageAwsPath = new Array(numFiles); 
+            imageAwsPath.fill(null);
             // debugger 
-            
-            
-            // req.files.map((item, idx) => {
-                for (let j = 0; j < req.files.length; j++) {
+
+
+            req.files.map((item, idx) => {
                 //setting params for aws
-                // arr.push(item);
+                arr.push(item);
                 var params = {
                     Bucket: process.env.AWS_BUCKET_NAME || keys.AWS_BUCKET_NAME,
-                    Key: req.files[j].originalname,
-                    Body: fs.createReadStream(req.files[j].path),
+                    Key: item.originalname,
+                    Body: fs.createReadStream(item.path),
                     ACL: 'public-read'
                 }
                 // debugger
@@ -58,125 +60,52 @@ router.post("/", [passport.authenticate('jwt', { session: false }), upload.array
                         res.json({ "error": true, "Message": err });
                     } else {
                         // success
-                        // arg = data[j]
-                        // debugger
-                        imageAwsPath.push(data.Location)
-                        // imageAwsPath[j] = data.Location;  
-                        
-                        // debugger
-                        if (numFiles === imageAwsPath.length) {
+
+                        // imageAwsPath.push(data.Location)
+                        imageAwsPath[idx] = data.Location;
+                        debugger
+                        if (numFiles === imageAwsPath.length && !imageAwsPath.includes(null)) {
                             // debugger
                             let ele = 0;
                             for (let i = 0; i < numFiles; i++) {
                                 // debugger
                                 resemble(imageAwsPath[i])
-                                .compareTo(playHunt.photo_collection[i])
-                                .scaleToSameSize()
-                                .ignoreAntialiasing()
-                                .onComplete(function(data) {
-                                    // debugger
-                                    // debugger
-                                    ele += 100 - data.misMatchPercentage
-                                    if (numFiles - i === 1) {
-                                        
-                                        const playHunt = new PlayHunt({
-                                            user: req.user.id,
-                                            hunt_id: req.body.hunt_id,
-                                            timestamps: req.body.timestamps,
-                                            score: ele,
-                                            images: imageAwsPath
-                                        })
-                                        
-                                        console.log(ele);
-                                        playHunt.save().then(playHunt => res.json(playHunt));
-                                    }
-                                    // break;
-                                })
+                                    .compareTo(playHunt.photo_collection[i])
+                                    .scaleToSameSize()
+                                    .ignoreAntialiasing()
+                                    .onComplete(function (data) {
+                                        debugger
+                                        // debugger
+                                        ele += 100 - data.misMatchPercentage;
+                                        if (numFiles - i === 1) {
+                                            debugger
+                                            const playHunt = new PlayHunt({
+                                                user: req.user.id,
+                                                hunt_id: req.body.hunt_id,
+                                                timestamps: req.body.timestamps,
+                                                score: ele / numFiles,
+                                                images: imageAwsPath
+                                            })
+                                            console.log(ele);
+                                            playHunt.save().then(playHunt => res.json(playHunt));
+                                        }
+                                        // break;
+                                    })
                             }
-                            
+
                             // playHunt.score
 
                         }
-                        
+
 
                     }
 
                 })
-            };
+            });
 
         })
-        
+
 
 })
 
 module.exports = router;
-
-
-
-// const express = require("express");
-// const router = express.Router();
-// const passport = require("passport");
-// const mongoose = require("mongoose");
-// const validatePlayHuntInput = require("../../validation/play_hunt");
-// const PlayHunt = require("../../models/PlayHunt");
-// var AWS = require("aws-sdk");
-// var multer = require('multer');
-// var upload = multer({ dest: 'uploads/' });
-// const keys = require('../../config/keys_dev');
-// var fs = require('file-system');
-// var fsy = require("fs");
-// var resemble = require('resemblejs');
-// var http = require("http");
-
-// router.get("/test", (req, res) => res.json({ msg: "This is the play hunts route" }));
-
-// router.post("/", [passport.authenticate('jwt', { session: false }), upload.array('images', 10)], (req, res) => {
-//     let arr = [];
-//     Hunt.findById(req.body.hunt_id)
-//         .then(hunting => {
-//             const { errors, isValid } = validatePlayHuntInput(req, hunting);
-//             if (!isValid) return res.status(400).json(errors);
-
-//             let s3bucket = new AWS.S3({
-//                 accessKeyId: process.env.AWS_ACCESS_KEY_ID || keys.AWS_ACCESS_KEY_ID,
-//                 secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || keys.AWS_SECRET_ACCESS_KEY,
-//                 region: process.env.AWS_REGION || keys.AWS_REGION
-//             });
-
-//             const numFiles = req.files.length;
-//             const imageAwsPath = [];
-//             let ele = 0;
-//             req.files.map((item, idx) => {
-//                 arr.push(item);
-//                 var params = {
-//                     Bucket: process.env.AWS_BUCKET_NAME || keys.AWS_BUCKET_NAME,
-//                     Key: item.originalname,
-//                     Body: fs.createReadStream(item.path),
-//                     ACL: 'public-read'
-//                 }
-
-//                 s3bucket.upload(params, function (err, data) {
-//                     if (err) {
-//                         res.json({ "error": true, "Message": err });
-//                     } else {
-//                         imageAwsPath.push(data.Location);
-//                         debugger
-//                         // if (numFiles === imageAwsPath.length) {
-//                             resemble(imageAwsPath[idx])
-//                                 .compareTo(hunting.photo_collection[idx])
-//                                 .scaleToSameSize()
-//                                 .ignoreAntiaLiasing()
-//                                 .onComplete(function (data) {
-//                                     ele += (100 - dataMisMatchPercentage);
-//                                 })
-//                             console.log(ele);
-//                         // }
-
-//                     }
-//                 })
-//             })
-
-//         })
-// })
-
-// module.exports = router;
