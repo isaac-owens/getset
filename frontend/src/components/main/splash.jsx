838d5cc3dbfd4d1bbe62749ffffcab854f346f8e
Isaac Owens <isaac.owens832@gmail.com>
2020-06-18 18:58:51 -0400

    Remove icons from splash

--- frontend/src/components/main/splash.jsx
+++ frontend/src/components/main/splash.jsx
@@ -7,8 +7,8 @@ import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 class Splash extends React.Component {
 
     render() {
-        let forwardIcon = <FontAwesomeIcon icon={faChevronRight} size="8x" />;
-        let backIcon = <FontAwesomeIcon icon={faChevronLeft} size="8x" />;
+        let forwardIcon = <FontAwesomeIcon icon={faChevronRight} size="x" />;
+        let backIcon = <FontAwesomeIcon icon={faChevronLeft} size="x" />;
     
         return (
           <div className="splash">
@@ -16,9 +16,9 @@ class Splash extends React.Component {
               <h1>Welcome to GetSet!</h1>
             </div>
             <div className="splash-body">
-                <div className="splash-icon left">{backIcon}</div>
-                <div className="splash-image">image here</div>
-                <div className="splash-icon right">{forwardIcon}</div>
+              <div className="splash-image">
+                image here
+              </div>
             </div>
           </div>
         );
