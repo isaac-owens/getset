f9649728d32e57746d51b472ecb35aede7017e33
Isaac Owens <isaac.owens832@gmail.com>
2020-06-18 02:04:06 -0400

    Start challenge index page without any animations or scroll functionality

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { 
-    AuthRoute, ProtectedRoute, SPLASH, AUTH, DASH, INDEX, CREATE 
+    AuthRoute, ProtectedRoute, SPLASH, AUTH, DASH, INDEX, CREATE, CINDEX 
 } from '../util/route_util';
 import { Route, Switch, Link } from 'react-router-dom';
 import CreateFormContainer from './hunt/create_form_container';
@@ -12,6 +12,7 @@ import Splash from './main/splash';
 import SessionFormContainer from './session/session_form_container';
 import DashboardContainer from './dashboard/dashboard_contianer';
 import HuntsIndexContainer from './hunt/hunts_index_container';
+import ChallengeIndexContainer from './challenge/challenge_index_container';
 
 const App = () => (
     <div className='app'>
@@ -22,6 +23,7 @@ const App = () => (
                 <Route exact path={CREATE} component={CreateFormContainer} />
                 <Route exact path={INDEX} component={HuntsIndexContainer} />
                 <ProtectedRoute exact path={DASH} component={DashboardContainer} />
+                <Route exact path={CINDEX} component={ChallengeIndexContainer} />
             </Switch>
          <Footer />
     </div>
---
+++ frontend/src/components/challenge/challenge_index_container.js
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux';
+import ChallengeIndexPage from './challenge_index';
+
+// const mSTP = state =>(
+//     {
+ 
+//     }
+// )
+
+// const mDTP = dispatch =>(
+//     {
+//         fetchChallenges: challenges => dispatch(fetchChallenges(challenges)),
+//         fetchChallenge: challenge => dispatch(fetchChallenge(challenge))
+//     }
+// )
+
+export default connect(null, null)(ChallengeIndexPage);
