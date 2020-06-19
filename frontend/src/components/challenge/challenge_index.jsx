79ec4705ccae0037dd20c78e445cd2be16eb4920
Zohaib Khaliq <khaliqzohaib92@gmail.com>
2020-06-19 14:33:21 -0400

    Complete User challenges routes and integration to frontend

--- frontend/src/components/challenge/challenge_index.jsx
+++ frontend/src/components/challenge/challenge_index.jsx
@@ -24,6 +24,7 @@ class ChallengeIndexPage extends React.Component {
   toggleMyChallage(toggle){
 
     return e=>{
+      debugger
       if(toggle == "add"){
         this.props.addToMyChallenges(this.state.selectedChallenge._id);
       }else{
