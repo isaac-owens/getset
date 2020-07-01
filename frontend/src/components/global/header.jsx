cb621409812d3268a89be0529c23e787be5ffc10
Isaac Owens <isaac.owens832@gmail.com>
2020-07-01 18:46:01 -0400

    Add styling to dropdown menu

--- frontend/src/components/global/header.jsx
+++ frontend/src/components/global/header.jsx
@@ -20,43 +20,44 @@ class Header extends React.Component {
       } else if (this.props.currentUser) {
               return (
                 <div>
-                  <ul className='drop-down'>
-                    <li>
-                      <div className="header-hello">
-                        {this.props.currentUser.username[0].toUpperCase()}
-                      </div>
-                      <ul className="header-nav-links sub-drop-down">
-                        <div>
-                          <li>
-                            <Link to={CREATE}
-                            className="header-nav-link">Create-a-Hunt</Link>
-                          </li>
-                          <li>
-                            <Link to={CINDEX}
-                            className="header-nav-link">FindChallenges</Link>
-                          </li>
-                          <li>
-                            <Link to={INDEX}
-                            className="header-nav-link">MyHunts</Link>
-                          </li>
-                          <li>
-                            <Link to={MYCHALL}
-                            className="header-nav-link">MyChallenges</Link>
-                          </li>
-                          <li>
-                            <Link to={STATS}
-                              className="header-nav-link">MyStats
-                            </Link>
-                          </li>
-                          <li className="header-button-container">
-                            <button onClick={this.logoutUser} className="session-button">
-                              Logout
-                            </button>
-                          </li>
-                        </div>
-                      </ul>
-                    </li>
-                  </ul>
+                  <div className='drop-down-trigger'>
+                    <button className='session-button'>Hi {this.props.currentUser.username}!</button>
+                  </div>
+                    <ul className="header-nav-links">
+                      <li className="header-nav-link-container">
+                          <Link 
+                           to={CREATE}
+                           className="header-nav-link"
+                          >Create-a-Hunt</Link>
+                      </li>
+                      <li className="header-nav-link-container">
+                          <Link to={CINDEX}
+                          className="header-nav-link"
+                          >FindChallenges</Link>
+                      </li>
+                      <li className="header-nav-link-container">
+                          <Link to={INDEX}
+                          className="header-nav-link"
+                          >MyHunts</Link>
+                      </li>
+                      <li className="header-nav-link-container">
+                          <Link to={MYCHALL}
+                          className="header-nav-link"
+                          >MyChallenges</Link>
+                      </li>
+                      <li className='header-nav-link-container'>
+                          <Link to={STATS}
+                            className="header-nav-link">
+                            MyStats</Link>
+                      </li>
+                    <li className='header-nav-link-container'>
+                        <Link to={SPLASH}
+                          className="header-nav-link"
+                          onClick={this.logoutUser}>
+                          Logout
+                        </Link>
+                      </li>
+                    </ul>
                 </div>
               );
           }
