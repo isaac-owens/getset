81504ffb82ef2e2b429650ff832c7978038c0a67
Isaac Owens <isaac.owens832@gmail.com>
2020-07-01 00:40:43 -0400

    Fix spacing in overflow scroll for hunts index list

--- frontend/src/components/hunt/hunt_collection_item.jsx
+++ frontend/src/components/hunt/hunt_collection_item.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 class HuntCollectionItem extends React.Component {
   constructor(props) {
@@ -9,15 +11,15 @@ class HuntCollectionItem extends React.Component {
 
   render() {
     // conditionally render class name based on props for styling
+    let redEx = <FontAwesomeIcon icon={faTimesCircle} size="2x"/>
     let { hunt, klassName } = this.props;
     klassName ? klassName = "mini-cards-active" : klassName = "mini-cards"
 
       return(
         hunt ? 
           <li  onClick={this.props.onCollectionClick}  className={klassName}>
-
             {hunt.title}
-
+            <div className='red-ex hunt-collection-ex'>{redEx}</div>
           </li> 
           : 
           <li  className={klassName}>
