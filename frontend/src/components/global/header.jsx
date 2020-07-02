6d2955d2da92e8f4610aad00c34ecd054eca0f19
Isaac Owens <isaac.owens832@gmail.com>
2020-07-01 21:21:01 -0400

    Complete dropdown menu with open/close functionality

--- frontend/src/components/global/header.jsx
+++ frontend/src/components/global/header.jsx
@@ -4,11 +4,22 @@ import { AUTH, CREATE, INDEX, STATS, CINDEX, SPLASH, MYCHALL } from '../../util/
 
 class Header extends React.Component {
     constructor(props) {
-        super(props);
-        this.logoutUser = this.logoutUser.bind(this);
-        this.getLinks = this.getLinks.bind(this);
+      super(props);
+      this.logoutUser = this.logoutUser.bind(this);
+      this.getLinks = this.getLinks.bind(this);
+      this.state = {
+        open: true
+      }
+      this.handleDropClick = this.handleDropClick.bind(this);
     }
 
+    handleDropClick = () => {
+      console.log('click');
+      this.setState({
+        open: !this.state.open
+      })
+    };
+    
     logoutUser(e) {
         e.preventDefault();
         this.props.logout();
@@ -18,68 +29,65 @@ class Header extends React.Component {
       if(this.props.location.pathname === AUTH){
         return (<div></div>);  
       } else if (this.props.currentUser) {
-              return (
-                <div>
-                  <div className='drop-down-trigger'>
-                    <button className='session-button'>Hi {this.props.currentUser.username}!</button>
-                  </div>
-                    <ul className="header-nav-links">
-                      <li className="header-nav-link-container">
-                          <Link 
-                           to={CREATE}
-                           className="header-nav-link"
-                          >Create-a-Hunt</Link>
-                      </li>
-                      <li className="header-nav-link-container">
-                          <Link to={CINDEX}
-                          className="header-nav-link"
-                          >FindChallenges</Link>
-                      </li>
-                      <li className="header-nav-link-container">
-                          <Link to={INDEX}
-                          className="header-nav-link"
-                          >MyHunts</Link>
-                      </li>
-                      <li className="header-nav-link-container">
-                          <Link to={MYCHALL}
-                          className="header-nav-link"
-                          >MyChallenges</Link>
-                      </li>
-                      <li className='header-nav-link-container'>
-                          <Link to={STATS}
-                            className="header-nav-link">
-                            MyStats</Link>
-                      </li>
-                    <li className='header-nav-link-container'>
-                        <Link to={SPLASH}
-                          className="header-nav-link"
-                          onClick={this.logoutUser}>
-                          Logout
-                        </Link>
-                      </li>
-                    </ul>
-                </div>
-              );
-          }
-        else {
-            return (
-              <div className="header-nav-links">
-                <Link to={AUTH}>SignUp | SignIn</Link>
+          return (
+            <div>
+              <div className="drop-down-trigger">
+                <button onClick={this.handleDropClick} className="session-button"
+                >
+                  Hi {this.props.currentUser.username}!
+                </button>
               </div>
-            );
-        }
-    }
-
-    render() {
-        return (
-            <div className='header'>
-                <Link to={SPLASH}>
-                    <h1 className='header-logo'>GetSet</h1>
-                </Link>
-                {this.getLinks()}
+              {this.state.open ? (
+                <ul className="header-nav-links">
+                  <Link to={CREATE} className="header-nav-link">
+                    <li className="header-nav-link-container">Create-a-Hunt</li>
+                  </Link>
+                  <Link to={CINDEX} className="header-nav-link">
+                    <li className="header-nav-link-container">
+                      FindChallenges
+                    </li>
+                  </Link>
+                  <Link to={INDEX} className="header-nav-link">
+                    <li className="header-nav-link-container">MyHunts</li>
+                  </Link>
+                  <Link to={MYCHALL} className="header-nav-link">
+                    <li className="header-nav-link-container">MyChallenges</li>
+                  </Link>
+                  <Link to={STATS} className="header-nav-link">
+                    <li className="header-nav-link-container">MyStats</li>
+                  </Link>
+                  <Link
+                    to={SPLASH}
+                    className="header-nav-link"
+                    onClick={this.logoutUser}
+                  >
+                    <li className="header-nav-link-container">Logout</li>
+                  </Link>
+                </ul>
+              ) : (
+                <div></div>
+              )}
             </div>
-        );
-    }
+          );
+        }else {
+          return (
+            <div className="header-nav-links">
+              <Link to={AUTH}>SignUp | SignIn</Link>
+            </div>
+          );
+        }
+  }
+
+  render() {
+      return (
+          <div className='header'>
+              <Link to={SPLASH}>
+                  <h1 className='header-logo'>GetSet</h1>
+              </Link>
+              {this.getLinks()}
+          </div>
+      );
+  }
 }
 
 export default Header;
