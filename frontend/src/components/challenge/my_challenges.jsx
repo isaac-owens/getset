c56e2518436419c38ff590e99ace16c1300f6f56
Kevin.K <kevinkusky@gmail.com>
2020-07-08 16:35:36 -0400

    Fixes display on empty mychallenges dropzone

--- frontend/src/components/challenge/my_challenges.jsx
+++ frontend/src/components/challenge/my_challenges.jsx
@@ -195,10 +195,11 @@ class MyChallenges extends React.Component {
                     </li>
                   )
                 }):
-
                   <li className="comparison-zone">
                       <div className="my-challenges-photo">image</div>
-                      <div className="my-challenges-drop-zone"><span className="my-challenges-drop-zone-message">drop zone component</span></div>
+                      <div className="my-challenges-drop-zone">
+                                            <span className="my-challenges-drop-zone-message">Drag'n'drop photo here, or click to select photoFile</span>
+                      </div>
                   </li>
               }
             </ul>
