17a53275ebf6dd90bb8455169ecf7d2a66d3226f
Isaac Owens <isaac.owens832@gmail.com>
2020-07-09 15:31:19 -0400

    Add name headers to meet the team page images

--- frontend/src/components/team/team.jsx
+++ frontend/src/components/team/team.jsx
@@ -5,20 +5,24 @@ export const Team = () => {
     return(
         <div className="team-page">
           <div className="member-about">
+            <span>Isaac</span>
             <img src={process.env.PUBLIC_URL + '/isaac.jpg'} alt=""  className="team-image" />
-            <div className="card-styling team-about">Isaac</div>
+            <div className="card-styling team-about"></div>
           </div>
           <div className="member-about">
+            <span>Kevin</span>
             <img src={process.env.PUBLIC_URL + '/kevin.png'} alt=""  className="team-image" />
-            <div className="card-styling team-about">Kevin</div>
+            <div className="card-styling team-about"></div>
           </div>
           <div className="member-about">
+            <span>Zohaib</span>
             <img src={process.env.PUBLIC_URL + '/zohaib.png'} alt=""  className="team-image" />
-            <div className="card-styling team-about">Zohaib</div>
+            <div className="card-styling team-about"></div>
           </div>
           <div className="member-about">
+            <span>Danny</span>
             <img src={process.env.PUBLIC_URL + '/danny.png'} alt=""  className="team-image" />
-            <div className="card-styling team-about">Danny</div>
+            <div className="card-styling team-about"></div>
           </div>
         </div>
     )
