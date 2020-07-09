d7d8cd9f4b155ceb32fbe9059308dd1443907611
Isaac Owens <isaac.owens832@gmail.com>
2020-07-09 00:59:04 -0400

    Resolve warnings in console

--- frontend/src/components/team/team.jsx
+++ frontend/src/components/team/team.jsx
@@ -5,19 +5,19 @@ export const Team = () => {
     return(
         <div className="team-page">
           <div className="member-about">
-            <img src={process.env.PUBLIC_URL + '/isaac.jpg'}  className="team-image" />
+            <img src={process.env.PUBLIC_URL + '/isaac.jpg'} alt=""  className="team-image" />
             <div className="card-styling team-about">Isaac</div>
           </div>
           <div className="member-about">
-            <img src={process.env.PUBLIC_URL + '/kevin.png'}  className="team-image" />
+            <img src={process.env.PUBLIC_URL + '/kevin.png'} alt=""  className="team-image" />
             <div className="card-styling team-about">Kevin</div>
           </div>
           <div className="member-about">
-            <img src={process.env.PUBLIC_URL + '/zohaib.png'}  className="team-image" />
+            <img src={process.env.PUBLIC_URL + '/zohaib.png'} alt=""  className="team-image" />
             <div className="card-styling team-about">Zohaib</div>
           </div>
           <div className="member-about">
-            <img src={process.env.PUBLIC_URL + '/danny.png'}  className="team-image" />
+            <img src={process.env.PUBLIC_URL + '/danny.png'} alt=""  className="team-image" />
             <div className="card-styling team-about">Danny</div>
           </div>
         </div>
