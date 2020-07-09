a127ba82b5b5a73072385f04f41bdcf432c607f3
Isaac Owens <isaac.owens832@gmail.com>
2020-07-08 23:15:52 -0400

    Change default due date for testing on stats winner

--- models/Hunt.js
+++ models/Hunt.js
@@ -23,7 +23,8 @@ const HuntSchema = new Schema({
     close_date: {
         type: Date,
         required: true,
-        default: () => new Date(+new Date() + 7 * 24 * 60 * 60 * 1000) 
+        default: new Date(),
+        // default: () => new Date(+new Date() + 7 * 24 * 60 * 60 * 1000) 
         //defaults to add a week from now
     },
     photo_collection: {
