66d0e9f996006350ebeaf235feb240c45d42e301
Isaac Owens <isaac.owens832@gmail.com>
2020-07-08 23:23:06 -0400

    Test challenge stats upload order on stats page

--- models/Hunt.js
+++ models/Hunt.js
@@ -23,9 +23,8 @@ const HuntSchema = new Schema({
     close_date: {
         type: Date,
         required: true,
-        default: new Date(),
-        // default: () => new Date(+new Date() + 7 * 24 * 60 * 60 * 1000) 
-        //defaults to add a week from now
+        default: () => new Date(+new Date() + 7 * 24 * 60 * 60 * 1000), 
+        // defaults to add a week from now
     },
     photo_collection: {
         type: Array,
