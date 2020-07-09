24294636fec98a126442d8191edb31406ae39da4
Isaac Owens <isaac.owens832@gmail.com>
2020-07-09 15:56:49 -0400

    Add icons for everyone on team page

--- frontend/src/components/team/team.jsx
+++ frontend/src/components/team/team.jsx
@@ -9,33 +9,61 @@ export const Team = () => {
   let github = <FontAwesomeIcon icon={faGithub} size="2x" />;    
   let linkedin = <FontAwesomeIcon icon={faLinkedin} size="2x" />;    
   let personalSite = <FontAwesomeIcon icon={faUserTie} size="2x" />;    
-  return(
-        <div className="team-page">
-          <div className="member-about">
-            <span>Isaac</span>
-            <img src={process.env.PUBLIC_URL + '/isaac.jpg'} alt=""  className="team-image" />
-            <div className="card-styling team-about"></div>
-          </div>
-          <div className="member-about">
-            <span>Kevin</span>
-            <img src={process.env.PUBLIC_URL + '/kevin.png'} alt=""  className="team-image" />
-            <div className="card-styling team-about">
-              {github}
-              {linkedin}
-              {personalSite}
-            </div>
-          </div>
-          <div className="member-about">
-            <span>Zohaib</span>
-            <img src={process.env.PUBLIC_URL + '/zohaib.png'} alt=""  className="team-image" />
-            <div className="card-styling team-about"></div>
-          </div>
-          <div className="member-about">
-            <span>Danny</span>
-            <img src={process.env.PUBLIC_URL + '/danny.png'} alt=""  className="team-image" />
-            <div className="card-styling team-about"></div>
-          </div>
+  return (
+    <div className="team-page">
+      <div className="member-about">
+        <span>Isaac</span>
+        <img
+          src={process.env.PUBLIC_URL + "/isaac.jpg"}
+          alt=""
+          className="team-image"
+        />
+        <div className="card-styling team-about">
+          {github}
+          {linkedin}
+          {personalSite}
         </div>
-    )
+      </div>
+      <div className="member-about">
+        <span>Kevin</span>
+        <img
+          src={process.env.PUBLIC_URL + "/kevin.png"}
+          alt=""
+          className="team-image"
+        />
+        <div className="card-styling team-about">
+          {github}
+          {linkedin}
+          {personalSite}
+        </div>
+      </div>
+      <div className="member-about">
+        <span>Zohaib</span>
+        <img
+          src={process.env.PUBLIC_URL + "/zohaib.png"}
+          alt=""
+          className="team-image"
+        />
+        <div className="card-styling team-about">
+          {github}
+          {linkedin}
+          {personalSite}
+        </div>
+      </div>
+      <div className="member-about">
+        <span>Danny</span>
+        <img
+          src={process.env.PUBLIC_URL + "/danny.png"}
+          alt=""
+          className="team-image"
+        />
+        <div className="card-styling team-about">
+          {github}
+          {linkedin}
+          {personalSite}
+        </div>
+      </div>
+    </div>
+  );
 };
 
