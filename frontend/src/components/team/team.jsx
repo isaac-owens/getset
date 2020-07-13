9fc1fe84ab2d85a9fcffe04e28d18632240310b9
Kevin.K <kevinkusky@gmail.com>
2020-07-13 16:49:25 -0400

    Cleans import statements

--- frontend/src/components/team/team.jsx
+++ frontend/src/components/team/team.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import {connect} from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
