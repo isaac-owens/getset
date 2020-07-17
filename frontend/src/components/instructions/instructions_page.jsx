74922c41bc8a19ec9e4b26176ad127f9177529f2
Isaac Owens <isaac.owens832@gmail.com>
2020-07-17 17:09:09 -0400

    Add gif to instruction page

--- frontend/src/components/instructions/instructions_page.jsx
+++ frontend/src/components/instructions/instructions_page.jsx
@@ -4,10 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Instructions = () => {
   return (
-    <div className="instructions-page">
-      <div className="instructions-sidebar"></div>
-      <div className="instructions-gif"></div>
-      <div className="instructions-content"></div>
+    <div className="instructions-page card-styling">
+      <div className="instructions-sidebar">SIDEBAR</div>
+      <div className="instructions-gif">
+        <img src={process.env.PUBLIC_URL + "/create.gif"} alt="" height="100%" width="1200px"/>
+      </div>
+      <div className="instructions-content card-styling">CONTENT</div>
     </div>
   )
 }
