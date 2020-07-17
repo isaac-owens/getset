764191e0e8965d8e7e6d9d40b51a036974e1f069
Isaac Owens <isaac.owens832@gmail.com>
2020-07-17 16:48:40 -0400

    Add styling blocks for spacing

--- frontend/src/components/instructions/instructions_page.jsx
+++ frontend/src/components/instructions/instructions_page.jsx
@@ -4,10 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Instructions = () => {
   return (
-    <div className="instruction-page">
-      <div className="instruction-sidebar"></div>
-      <div className="instruction-gif"></div>
-      <div className="instruction-content"></div>
+    <div className="instructions-page">
+      <div className="instructions-sidebar"></div>
+      <div className="instructions-gif"></div>
+      <div className="instructions-content"></div>
     </div>
   )
 }
