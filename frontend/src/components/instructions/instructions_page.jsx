3a348707252630babaac79b53703470134c000a0
Isaac Owens <isaac.owens832@gmail.com>
2020-07-17 13:58:48 -0400

    Add structure for instruction page

---
+++ frontend/src/components/instructions/instructions_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+export const Instructions = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Instructions;
