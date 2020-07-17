2ed2996b7f01273a6fc8d67198b8cee6aaa2741b
Isaac Owens <isaac.owens832@gmail.com>
2020-07-16 20:12:04 -0400

    Add redEx icon to create modal

--- frontend/src/components/hunt/create_modal.jsx
+++ frontend/src/components/hunt/create_modal.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { INDEX } from '../../util/route_util';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 const CreateModal = () => {
+  let redEx = <FontAwesomeIcon icon={faTimesCircle} size="2x" />
+
   return (
     <div className="create-modal">
-      <span className="create-modal-ex">X</span>
+      <span className="create-modal-ex">{redEx}</span>
       <span className="create-modal-content card-styling">
         You successfully created a Hunt! To see your hunt visit  <Link to={INDEX} className="create-modal-link"> My Hunts!</Link>
       </span>
