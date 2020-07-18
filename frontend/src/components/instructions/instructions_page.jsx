a85f863d40bd66647cb9900ec0d0ce76bcfb39e4
zohaib <khaliqzohaib92@gmail.com>
2020-07-17 22:28:12 -0400

    Add all the instructions for instructions page, also tested the dynamically data population of the instruction page

--- frontend/src/components/instructions/instructions_page.jsx
+++ frontend/src/components/instructions/instructions_page.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {data}  from '../../util/instructions_data'
-export const Instructions = () => {
-  return (
-    <div className="instructions-page">
-      <div className="instructions-sidebar card-styling">
-        <ul>
-          {
-            Object.entries(data).map(([key,value])=>{
-              return <li>{value.name}</li>
-            })
-          }
-        </ul>
-      </div>
-      <div className="instructions-gif">
-        <img src={process.env.PUBLIC_URL + "/create.gif"} alt="" height="100%" width="1200px"/>
-      </div>
-      <div className="instructions-content card-styling">CONTENT</div>
-    </div>
-  )
+import React, { Component } from 'react';
+import {data}  from '../../util/instructions_data';
+
+export default class Instructions extends Component { 
+  constructor(props){
+    super(props);
+
+    this.state = {selectedPage : 0}
+  }
+
+  onPageSelected = (pageIdx) => {
+    return e => {
+      this.setState({selectedPage : pageIdx});
+    }
+  }
+  
+  render(){
+    const dataArr = Object.values(data);
+      return (
+        <div className="instructions-page">
+          <div className="instructions-sidebar card-styling">
+            <ul>
+              {
+                Object.entries(data).map(([key,value])=>{
+                  return <li key={key} onClick={this.onPageSelected(key)}>{value.name}</li>
+                })
+              }
+            </ul>
+          </div>
+          <div className="instructions-gif">
+            <img src={data[this.state.selectedPage].gifUrl} alt="" height="100%" width="1200px"/>
+          </div>
+          <div className="instructions-content card-styling">{data[this.state.selectedPage].instructions}</div>
+        </div>
+      ) 
+  }
 }
 
-export default Instructions;
--- frontend/src/util/instructions_data.js
+++ frontend/src/util/instructions_data.js
@@ -6,21 +6,44 @@ export const data = {
         gifUrl: process.env.PUBLIC_URL + "/create.gif",
         instructions: 
         <ul>
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-            <li>Four</li>
-            <li>Five</li>
+            <li>1. Create a title</li>
+            <li>2. Upload your pictures</li>
+            <li>3. Choose a category</li>
+            <li>4. Click 'Set Game' button to create</li>
+            <li>5. Go to 'MyHunts to find your new hunt!</li>
         </ul>
     },
     1: {
-        name: "Create a Hunt",
+        name: "Find a Challenge",
         gifUrl: "url here...",
-        instructions: "-one \n -two"
+        instructions:
+          <ul>
+            <li>1. Choose a category</li>
+            <li>2. Select a challenge from the menu</li>
+            <li>3. Click 'Add challenge' to accept challenge</li>
+            <li>4. Click 'Remove challenge' to remove challenge</li>
+            <li>5. Go to 'MyChallenges to see your accepted challenges!</li>
+          </ul>
     },
     2: {
-        name: "Create a Hunt",
+        name: "Complete a Challenge",
         gifUrl: "url here...",
-        instructions: "-one \n -two"
+        instructions:
+          <ul>
+            <li>1. Select a challenge from the side menu</li>
+            <li>2. Upload your matches to the challenge pictures</li>
+            <li>3. Submit your challenge to get your score</li>
+            <li>4. Receive your score and head to stats!</li>
+          </ul>
+    },
+    3: {
+        name: "Read your Stats",
+        gifUrl: "url here...",
+        instructions:
+          <ul>
+            <li>1. Find your challenge in the 'Challenge' column</li>
+            <li>2. You'll find your score in the middle column</li>
+            <li>3. A winner is announced on the challenge's deadline date</li>
+          </ul>
     }
 }
