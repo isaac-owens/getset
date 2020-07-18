7dc80c6617e17c4b2313f7cd027a4996f13b33f2
zohaib <khaliqzohaib92@gmail.com>
2020-07-17 21:55:57 -0400

    Make data for instruction component

--- frontend/src/components/instructions/instructions_page.jsx
+++ frontend/src/components/instructions/instructions_page.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
 export const Instructions = () => {
   return (
     <div className="instructions-page">
-      <div className="instructions-sidebar card-styling">SIDEBAR</div>
+      <div className="instructions-sidebar card-styling">
+        <ul>
+          <li></li>
+        </ul>
+      </div>
       <div className="instructions-gif">
         <img src={process.env.PUBLIC_URL + "/create.gif"} alt="" height="100%" width="1200px"/>
       </div>
---
+++ frontend/src/util/instructions_data.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const data = {
+    0: {
+        name: "Create a Hunt",
+        gifUrl: process.env.PUBLIC_URL + "/create.gif",
+        instructions: 
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+            <li>Four</li>
+            <li>Five</li>
+        </ul>
+    },
+    1: {
+        name: "Create a Hunt",
+        gifUrl: "url here...",
+        instructions: "-one \n -two"
+    },
+    2: {
+        name: "Create a Hunt",
+        gifUrl: "url here...",
+        instructions: "-one \n -two"
+    }
+}
