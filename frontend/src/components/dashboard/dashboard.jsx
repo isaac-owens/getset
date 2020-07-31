a2449b3359f7db8062a385cd2513c902db46c44b
Isaac Owens <isaac.owens832@gmail.com>
2020-07-31 13:19:02 -0400

    Fix meet the team no being clickable on dashboar page

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -8,11 +8,6 @@ class Dashboard extends Component {
             <div className="card card-styling one"></div>
             <div className="card card-styling two"></div>
             <div className="card card-styling three">
-                {/* <p className='welcome message'>
-                    Thank You for visiting Get Set - the Game we take your photo skills to the test
-                    Create Your own games!, Challenge Your friends!  GetSet to have fun!
-                    
-                </p> */}
               <ul className="scroll-container">
                 <li>
                   <img src={process.env.PUBLIC_URL + '/cheetah.png'} alt="" />
