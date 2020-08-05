fe71545dc5d120e36c37b80e246d049d45d8fb2b
Kevin.K <kevinkusky@gmail.com>
2020-08-05 14:05:53 -0400

    Removes unused files and cleans semicolons

--- frontend/src/components/challenge/challenge_index_container.js
+++ frontend/src/components/challenge/challenge_index_container.js
@@ -3,21 +3,21 @@ import ChallengeIndexPage from './challenge_index';
 import {fetchChallenges, addToMyChallenge, deleteChallenge} from '../../actions/challenge_actions'
 import {fetchCategories} from '../../actions/category_actions'
 
-const mSTP = state =>{
-  return  {
+const mSTP = state => (
+    {
         challenges: state.challenges,
         categories: Object.values(state.categories),
         myChallenges: state.session.user.myChallenges ? state.session.user.myChallenges : []
     }
-}
+);
 
-const mDTP = dispatch =>(
+const mDTP = dispatch => (
     {
         fetchChallenges: () => dispatch(fetchChallenges()),
         addToMyChallenge: (id) => dispatch(addToMyChallenge(id)),
         deleteChallenge: (id) => dispatch(deleteChallenge(id)),
         fetchCategories: () => dispatch(fetchCategories())
     }
-)
+);
 
 export default connect(mSTP, mDTP)(ChallengeIndexPage);
--- frontend/src/components/hunt/create_form_container.js
+++ frontend/src/components/hunt/create_form_container.js
@@ -3,13 +3,13 @@ import{ createUserHunt } from '../../actions/user_hunt_actions';
 import CreateForm from './create_form';
 import {fetchCategories} from '../../actions/category_actions'
 
-const mSTP = state => {
-  return  {
+const mSTP = state => (
+    {
         currentUser: state.session.currentUser,
         categories: Object.values(state.categories),
         errors: state.errors.hunt,
     }
-};
+);
 
 const mDTP = dispatch => (
     {
